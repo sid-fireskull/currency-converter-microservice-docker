0f7457b88e11ddc667b64c64f7fd138dc5203845
Sid <siddhartha@dcpltech.com>
2024-07-29 22:51:24 +0530

    SQL Statement Added

--- Currency_Exchange_Service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
+++ Currency_Exchange_Service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -35,3 +35,13 @@ public class CurrencyExchange {
 	
 	private String environment;
 }
+
+//insert into currency_exchange
+//(currency_from,currency_to,conversion_multiple,environment)
+//values('USD','INR',65,'');
+//insert into currency_exchange
+//(currency_from,currency_to,conversion_multiple,environment)
+//values('EUR','INR',75,'');
+//insert into currency_exchange
+//(currency_from,currency_to,conversion_multiple,environment)
+//values('AUD','INR',25,'');
