41b95738792525d09b7d2495c49420af5013696f
Sid <siddhartha@dcpltech.com>
2024-07-29 22:45:13 +0530

    Config Server Working For Two Services

--- Currency_Exchange_Service/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into currency_exchange
-(currency_from,currency_to,conversion_multiple,environment)
-values('USD','INR',65,'');
-insert into currency_exchange
-(currency_from,currency_to,conversion_multiple,environment)
-values('EUR','INR',75,'');
-insert into currency_exchange
-(currency_from,currency_to,conversion_multiple,environment)
-values('AUD','INR',25,'');
