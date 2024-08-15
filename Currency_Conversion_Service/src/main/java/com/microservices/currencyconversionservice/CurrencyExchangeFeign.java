13425fea9b610ec55e64c51464e8b2d2079d093c
Sid <siddhartha@dcpltech.com>
2024-08-15 15:02:29 +0530

    Naming Server Configured & Working with Fiegn

--- Currency_Conversion_Service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeFeign.java
+++ Currency_Conversion_Service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "http://localhost:8110")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeFeign {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
--- Currency_Naming_Server/src/main/java/com/microservices/namingserver/Currency_Naming_Server/CurrencyNamingServerApplication.java
+++ Currency_Naming_Server/src/main/java/com/microservices/namingserver/Currency_Naming_Server/CurrencyNamingServerApplication.java
@@ -2,7 +2,9 @@ package com.microservices.namingserver.Currency_Naming_Server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class CurrencyNamingServerApplication {
 
